5740ff0ae773092960bc41395fd5c7000ab53fa1
gjaku1031 <gjaku1031@gmail.com>
2025-07-01 18:01:08 +0900

    init

---
+++ src/main/java/com/example/neo4j_mcp_test/Neo4jMcpTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.neo4j_mcp_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jMcpTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jMcpTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/neo4j_mcp_test/Neo4jMcpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neo4j_mcp_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jMcpTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
